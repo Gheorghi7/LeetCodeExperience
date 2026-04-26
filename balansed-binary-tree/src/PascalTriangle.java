c35508344fcf90cb245e30f0c0596fd17b193288
Gheorghi <valentina161012@gmail.com>
2026-04-26 21:39:28 +0300

    Pascal triangle

---
+++ balansed-binary-tree/src/PascalTriangle.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PascalTriangle {
+    static void main() {
+        System.out.println(generate(5));
+    }
+
+
+    public static List<Integer> generate(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+        long prev = 1;
+        for (int k = 1; k <= rowIndex; k++) {
+            long next_val = prev * (rowIndex - k + 1) / k;
+            res.add((int) next_val);
+            prev = next_val;
+        }
+        return res;
+    }
+}
+
+
+
+
