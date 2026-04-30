3a70fe8aca3f09bee84ed30ebdd160657c18f367
Gheorghi <valentina161012@gmail.com>
2026-04-30 22:00:23 +0300

    SingleNumberII

--- balansed-binary-tree/src/FindSIngleNumber.java
+++ balansed-binary-tree/src/FindSIngleNumber.java
@@ -1,20 +1,19 @@
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FindSIngleNumber {
     static void main() {
-        System.out.println(singleNumber(new int[]{4,1,2,1,2}));
+        System.out.println(singleNumber(new int[]{2, 2, 3, 2}));
     }
 
-    public static int singleNumber(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < nums.length; i++){
-            boolean b = !set.contains(nums[i]) ? set.add(nums[i]) : set.remove(nums[i]);
-        }
-
-
-        return set.iterator().next();
+        public static int singleNumber(int[] nums) {
+            int ones = 0, twos = 0;
+            for (int num : nums) {
+                ones = (ones ^ num) & ~twos;
+                twos = (twos ^ num) & ~ones;
+            }
+            return ones;
 
     }
 }
